78adae458a38e0635849048e358a195a337a974a
abeshilu <102710800+abeshilu@users.noreply.github.com>
2023-02-26 23:03:05 -0600

    idk

--- p3/src/App.js
+++ p3/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const API_KEY = 'YOUR_API_KEY_HERE';
+const API_BASE_URL = `https://www.alphavantage.co/query?apikey=${ API_KEY }`;
+const DEFAULT_INPUT = 'MSFT';
+
+function App ()
+{
+  const [ data, setData ] = useState( {} );
+  const [ input, setInput ] = useState( DEFAULT_INPUT );
+  const [ error, setError ] = useState( null );
+
+  useEffect( () =>
+  {
+    const fetchData = async () =>
+    {
+      try
+      {
+        const response = await fetch( `${ API_BASE_URL }&function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ input }` );
+        if ( !response.ok )
+        {
+          throw new Error( `HTTP error! status: ${ response.status }` );
+        }
+        const json = await response.json();
+        setData( json );
+        setError( null );
+      } catch ( error )
+      {
+        setData( {} );
+        setError( error.message );
+      }
+    };
+    fetchData();
+  }, [ input ] );
+
+  const handleInputChange = e =>
+  {
+    setInput( e.target.value );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Stock Viewer</h1>
+      <div>
+        <h2>Select Stock:</h2>
+        <button onClick={() => setInput( 'MSFT' )}>Microsoft</button>
+        <button onClick={() => setInput( 'AAPL' )}>Apple</button>
+        <button onClick={() => setInput( 'AMZN' )}>Amazon</button>
+        <div>
+          <label htmlFor="text-input">Enter Symbol:</label>
+          <input type="text" id="text-input" value={input} onChange={handleInputChange} />
+        </div>
+      </div>
+      <div>
+        <h2>Results:</h2>
+        {error && <div>{error}</div>}
+        {data[ 'Time Series (Daily)' ] && (
+          <ul>
+            {Object.entries( data[ 'Time Series (Daily)' ] ).map( ( [ date, datum ] ) => (
+              <li key={date}>
+                {date} - {datum[ '4. close' ]}
+              </li>
+            ) )}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
