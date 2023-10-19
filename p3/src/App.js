7c0a11caf012ccd57b2702cab4ca286bff5c88f8
abeshilu <102710800+abeshilu@users.noreply.github.com>
2023-10-18 21:26:26 -0500

    added app.js

--- p3/src/App.js
+++ p3/src/App.js
@@ -1,71 +1,294 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/database";
 
-const API_KEY = 'YOUR_API_KEY_HERE';
-const API_BASE_URL = `https://www.alphavantage.co/query?apikey=${ API_KEY }`;
-const DEFAULT_INPUT = 'MSFT';
 
 function App ()
 {
-  const [ data, setData ] = useState( {} );
-  const [ input, setInput ] = useState( DEFAULT_INPUT );
-  const [ error, setError ] = useState( null );
+  const [ symbol, setSymbol ] = useState( 'SPY' );
+  const [ results, setResults ] = useState( [] );
+  const [ error, setError ] = useState( '' );
+  const [ symbols, setSymbols ] = useState( [] );
+  const [ savedSymbols, setSavedSymbols ] = useState( [ 'GOOGL', 'SPY', 'AAPL', 'TSLA' ] );
+  const [ user, setUser ] = useState( null );
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAINycS4U_qtwiNm1STVoD-WHkYj5aY7b4",
+    authDomain: "cs378-p4-a1a24.firebaseapp.com",
+    databaseURL: "https://cs378-p4-a1a24-default-rtdb.firebaseio.com",
+    projectId: "cs378-p4-a1a24",
+    storageBucket: "cs378-p4-a1a24.appspot.com",
+    messagingSenderId: "1059173415739",
+    appId: "1:1059173415739:web:81aa4e8fb5aac7d82b951e",
+    measurementId: "G-35RQNYTHD2"
+  };
+
+  firebase.initializeApp( firebaseConfig );
+
+  const auth = firebase.auth();
+  const database = firebase.database();
+
+  const users = [
+    { email: 'user1@example.com', password: 'password1' },
+    { email: 'user2@example.com', password: 'password2' },
+    { email: 'user3@example.com', password: 'password3' }
+  ];
+
+  users.forEach( user =>
+  {
+    firebase.auth().createUserWithEmailAndPassword( user.email, user.password )
+      .then( ( userCredential ) =>
+      {
+        // User created successfully
+        const { uid } = userCredential.user;
+        firebase.database().ref( `users/${ uid }` ).set( {
+          email: user.email,
+          password: user.password
+        } );
+      } )
+      .catch( ( error ) =>
+      {
+        console.error( error );
+      } );
+  } );
+
+
+  const handleSignIn = async ( event ) =>
+  {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    try
+    {
+      const userCredential = await auth.signInWithEmailAndPassword( email, password );
+      setUser( userCredential.user );
+
+      // Retrieve application state for this user
+      const userRef = database.ref( `users/${ userCredential.user.uid }` );
+      userRef.on( "value", ( snapshot ) =>
+      {
+        const data = snapshot.val();
+        setSavedSymbols( data?.savedSymbols || [ 'GOOGL', 'SPY', 'AAPL', 'TSLA' ] );
+      } );
+
+    } catch ( error )
+    {
+      console.error( error );
+      setError( "Invalid username or password" );
+    }
+  };
+
+  const handleSignOut = () =>
+  {
+    const userRef = database.ref( `users/${ user?.uid }` );
+    userRef.set( { savedSymbols } );
+
+    auth.signOut();
+    setUser( null );
+    setSavedSymbols( [ 'GOOGL', 'SPY', 'AAPL', 'TSLA' ] );
+  };
 
   useEffect( () =>
   {
-    const fetchData = async () =>
+    const unsubscribe = auth.onAuthStateChanged( ( user ) =>
+    {
+      if ( user )
+      {
+        setUser( user );
+      } else
+      {
+        setUser( null );
+      }
+    } );
+
+    return () => unsubscribe();
+  }, [] );
+
+  const handleSaveSymbol = async ( event ) =>
+  {
+    event.preventDefault();
+    if ( !savedSymbols.includes( symbol ) )
+    {
+      setSavedSymbols( [ ...savedSymbols, symbol ] );
+    }
+    const userRef = database.ref( `users/${ user?.uid }` );
+    userRef.update( { savedSymbols: [ ...savedSymbols, symbol ] } );
+  };
+
+  useEffect( () =>
+  {
+    const fetchSymbols = async () =>
     {
       try
       {
-        const response = await fetch( `${ API_BASE_URL }&function=TIME_SERIES_DAILY_ADJUSTED&symbol=${ input }` );
-        if ( !response.ok )
-        {
-          throw new Error( `HTTP error! status: ${ response.status }` );
-        }
-        const json = await response.json();
-        setData( json );
-        setError( null );
+        const response = await fetch(
+          `https://datahub.io/core/s-and-p-500-companies/r/constituents.json`
+        );
+        const data = await response.json();
+        const symbols = data.map( ( company ) => company.Symbol );
+        setSymbols( symbols );
+        handleSubmit( { preventDefault: () => { } } );
       } catch ( error )
       {
-        setData( {} );
-        setError( error.message );
+        console.error( error );
       }
     };
-    fetchData();
-  }, [ input ] );
+    fetchSymbols();
+  }, [] );
 
-  const handleInputChange = e =>
+  const handleSubmit = async ( event ) =>
   {
-    setInput( e.target.value );
+    event.preventDefault();
+
+    if ( !symbols.includes( symbol ) && !( symbol === "SPY" ) )
+    {
+      setError( `Invalid stock symbol ${ symbol }` );
+      setResults( [] );
+      return;
+    }
+
+    try
+    {
+      const response = await fetch(
+        `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ symbol }&apikey=YOUR_API_KEY`
+      );
+      const data = await response.json();
+
+      if ( data[ 'Global Quote' ] )
+      {
+        setResults( Object.entries( data[ 'Global Quote' ] ) );
+        setError( '' );
+      } else
+      {
+        setError( `Max searches allowed at this time!` );
+        setResults( [] );
+      }
+    } catch ( error )
+    {
+      console.error( error );
+      setError( `An error occurred while fetching data for ${ symbol }` );
+      setResults( [] );
+    }
   };
+
+  const handleChange = ( event ) =>
+  {
+    setSymbol( event.target.value );
+  };
+
+  const handleButtonClick = async ( event ) =>
+  {
+    const buttonSymbol = event.target.value;
+    setSymbol( buttonSymbol );
+
+    try
+    {
+      const response = await fetch(
+        `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ buttonSymbol }&apikey=YOUR_API_KEY`
+      );
+      const data = await response.json();
+
+      if ( data[ 'Global Quote' ] )
+      {
+        setResults( Object.entries( data[ 'Global Quote' ] ) );
+        setError( '' );
+      } else
+      {
+        setError( `Max searches allowed at this time!` );
+        setResults( [] );
+      }
+    } catch ( error )
+    {
+      console.error( error );
+      setError( `An error occurred while fetching data for ${ buttonSymbol }` );
+      setResults( [] );
+    }
+  };
+
+  const handleSaveButtonClick = () =>
+  {
+    if ( !symbols.includes( symbol ) && !( symbol === "SPY" ) )
+    {
+      setError( `Invalid stock symbol ${ symbol }` );
+      setResults( [] );
+      return;
+    }
+    if ( !savedSymbols.includes( symbol ) )
+    {
+      setSavedSymbols( [ ...savedSymbols, symbol ] );
+    }
+  }
+
 
   return (
     <div>
-      <h1>Stock Viewer</h1>
-      <div>
-        <h2>Select Stock:</h2>
-        <button onClick={() => setInput( 'MSFT' )}>Microsoft</button>
-        <button onClick={() => setInput( 'AAPL' )}>Apple</button>
-        <button onClick={() => setInput( 'AMZN' )}>Amazon</button>
-        <div>
-          <label htmlFor="text-input">Enter Symbol:</label>
-          <input type="text" id="text-input" value={input} onChange={handleInputChange} />
+      {user ? (
+        <div class="centered">
+          <p>Welcome, {user.email}!</p>
+          <button onClick={handleSignOut}>Sign out</button>
         </div>
-      </div>
-      <div>
-        <h2>Results:</h2>
-        {error && <div>{error}</div>}
-        {data[ 'Time Series (Daily)' ] && (
-          <ul>
-            {Object.entries( data[ 'Time Series (Daily)' ] ).map( ( [ date, datum ] ) => (
-              <li key={date}>
-                {date} - {datum[ '4. close' ]}
-              </li>
-            ) )}
-          </ul>
-        )}
-      </div>
+      ) : (
+        <form onSubmit={handleSignIn}>
+          <label>
+            Email:
+            <input type="email" name="email" required />
+          </label>
+          <label>
+            Password:
+            <input type="password" name="password" required />
+          </label>
+          <button type="submit">Sign in</button>
+        </form>
+      )}
+      <h1>Stock Info Interface</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter a Stock Symbol in the S&P 500:
+        </label>
+        <input type="text" value={symbol} onChange={handleChange} />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleSaveSymbol || handleSaveButtonClick}>+</button>
+      </form>
+      <p>Click a button to automatically search for a stock symbol:</p>
+
+      {savedSymbols.length > 0 && (
+        <div class="centered">
+          {savedSymbols.map( ( symbol ) => (
+            <button key={symbol} onClick={() => handleButtonClick( { target: { value: symbol } } )}>
+              {symbol}
+            </button>
+          ) )}
+        </div>
+      )}
+      {results.length === 0 && !error && (
+        <p>Enter a stock symbol in the S&P 500 to get started</p>
+      )}
+      {error && <p>{error}</p>}
+      {results.length > 0 && (
+        <div class="divTable">
+          <table>
+            <thead>
+              <tr>
+                <th>Key</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {results.map( ( [ key, value ] ) => (
+                <tr key={key}>
+                  <td>{key}</td>
+                  <td>{value}</td>
+                </tr>
+              ) )}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
